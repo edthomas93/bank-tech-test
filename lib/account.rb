fcbf016a16a2a5e6cb4bdbec9b3829a0fe4666f8
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-11 11:30:28 +0100

    DRYed up withdraw and deposit methods, added tests for entering a string instead of value

--- lib/account.rb
+++ lib/account.rb
@@ -10,40 +10,29 @@ class Account
   end
 
   def date_of_transaction
-    t = Time.now
-    t.strftime('%d/%m/%Y')
+    date = DateTime.now
+    date.strftime('%d/%m/%Y')
   end
 
   def deposit(amount)
-    if decimals(amount) > 2
-      raise "You cannot deposit a fraction of a penny. Please enter an amount up to 2 decimal places"
-    elsif amount < 0
-      raise "Cannot deposit a negative value"
-    else
-      @balance += amount
-      @transactions.push(date: date_of_transaction, credit: format('%.2f', amount), debit: nil, balance: format('%.2f', @balance))
-    end
+    error_message(amount)
+    @balance += amount
+    @transactions.push(date: date_of_transaction, credit: format('%.2f', amount), debit: nil, balance: format('%.2f', @balance))
   end
 
   def withdraw(amount)
-    if decimals(amount) > 2
-      raise "You cannot withdraw a fraction of a penny. Please enter an amount up to 2 decimal places"
-    elsif amount < 0
-      raise "Cannot withdraw a negative value"
-    else
-      @balance -= amount
-      @transactions.push(date: date_of_transaction, credit: nil, debit: format('%.2f', amount), balance: format('%.2f', @balance))
-    end
+    error_message(amount)
+    @balance -= amount
+    @transactions.push(date: date_of_transaction, credit: nil, debit: format('%.2f', amount), balance: format('%.2f', @balance))
   end
 end
 
-private
-
-def decimals(number)
-    dp = 0
-    while(number != number.to_i)
-        dp += 1
-        number *= 10
-    end
-    dp
+def error_message(value)
+  if !value.is_a? Numeric
+    raise "Please enter a numeric value only"
+  elsif value*100 != (value*100).to_i
+    raise "Please enter an amount up to 2 decimal places"
+  elsif value < 0
+    raise "Cannot input a negative value"
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,10 @@ require 'account'
 
 describe Account do
   let(:account) { Account.new }
+  let(:date) { DateTime.now }
 
   before(:each) do
-    stub_date = '10/09/2018'
-    allow(DateTime).to receive(:now).and_return(stub_date)
+    allow(DateTime).to receive(:now).and_return(date)
   end
 
   it 'is expected to respond to balance' do
@@ -36,15 +36,19 @@ describe Account do
 
     it 'stores a hash in transactions with credit as a float and debit as nil' do
       account.deposit(500)
-      expect(account.transactions).to eq [{ date: '10/09/2018', credit: '500.00', debit: nil, balance: '500.00' }]
+      expect(account.transactions).to eq [{ date: date.strftime("%d/%m/%Y"), credit: '500.00', debit: nil, balance: '500.00' }]
     end
 
     it 'cannot be negative' do
-      expect{ account.deposit(-1) }.to raise_error 'Cannot deposit a negative value'
+      expect{ account.deposit(-1) }.to raise_error 'Cannot input a negative value'
     end
 
     it 'does not allow more than 2dp amount to be withdrawn' do
-      expect{ account.deposit(100.313) }.to raise_error 'You cannot deposit a fraction of a penny. Please enter an amount up to 2 decimal places'
+      expect{ account.deposit(100.313) }.to raise_error 'Please enter an amount up to 2 decimal places'
+    end
+
+    it 'must eneter numeric value' do
+      expect{ account.deposit('mulah') }.to raise_error 'Please enter a numeric value only'
     end
   end
 
@@ -57,25 +61,19 @@ describe Account do
 
     it 'stores a hash in transactions with debit as a float and credit as nil' do
       account.withdraw(500)
-      expect(account.transactions).to eq [{ date: '10/09/2018', credit: nil, debit: '500.00', balance: '-500.00' }]
+      expect(account.transactions).to eq [{ date: date.strftime("%d/%m/%Y"), credit: nil, debit: '500.00', balance: '-500.00' }]
     end
 
     it 'cannot be negative' do
-      expect{ account.withdraw(-1) }.to raise_error 'Cannot withdraw a negative value'
+      expect{ account.withdraw(-1) }.to raise_error 'Cannot input a negative value'
     end
 
     it 'does not allow more than 2dp amount to be withdrawn' do
-      expect{ account.withdraw(912.313) }.to raise_error 'You cannot withdraw a fraction of a penny. Please enter an amount up to 2 decimal places'
+      expect{ account.withdraw(912.313) }.to raise_error 'Please enter an amount up to 2 decimal places'
     end
-  end
 
-  describe '#decimals' do
-    it 'identifies the number of decimal places in a number' do
-      expect(decimals(0.1234)).to eq 4
-      expect(decimals(0.123)).to eq 3
-      expect(decimals(0.12)).to eq 2
-      expect(decimals(0.1)).to eq 1
-      expect(decimals(1)).to eq 0
+    it 'must eneter numeric value' do
+      expect{ account.withdraw('money') }.to raise_error 'Please enter a numeric value only'
     end
   end
 end
